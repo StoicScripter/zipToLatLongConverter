0e62176ac28a63d43fefbae6f94ae7a117fb5cef
Tom Gottselich <developer.gottselich@gmail.com>
2023-08-07 20:36:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This script converts zip codes in a csv file to latitude and longitude floats
+
+
+def convert():
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    convert()
+
